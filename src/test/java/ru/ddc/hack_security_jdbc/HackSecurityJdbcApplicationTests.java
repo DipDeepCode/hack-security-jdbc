a29997fd5a583a0c5f1a3b650e67559261e9b284
svkovalev <svkovalev@bank-hlynov.ru>
2024-05-24 14:03:31 +0300

    hsj :: init project

---
+++ src/test/java/ru/ddc/hack_security_jdbc/HackSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.hack_security_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackSecurityJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
